55a1e7d4a68b78b9d35af44603725337cb6d0f75
Hua <65344123+GatsbyHua@users.noreply.github.com>
2021-10-02 17:04:43 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <resource-home/>
+  </div>
+</template>
+
+
+<script>
+import ResourceHome from "./views/ResourceHome.vue";
+// import resourcesHome from "./views/ResourceHome.vue"
+// import initial_layout from "./resources/intial_layout.html"
+
+export default{
+  name:"App",
+  components:{
+    ResourceHome,
+  }
+}
+
+
+</script>
+
+<style>
+  /* <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous"> */
+</style>
--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# vue3-demo
-vue3-demo
+# vue3-demo1
+
+## Project setup
+
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Customize configuration
+
+See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### add css style 9.25
+
+1.add css link in public/index.html <head>
+
+2.
+cnpm install bootstrap@4.5.2 --save
+add in App.vue
+such as:
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
